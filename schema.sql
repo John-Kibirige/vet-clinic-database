2fa96505acc90dab31f6f9e352c3ad24b1527067
John <kibirigejohn005@gmail.com>
2022-10-19 08:44:14 +0300

    Add the schema for creating a table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
+ create table animals(
+    id integer generated always as identity,
+    name varchar(50),
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg decimal
 );
