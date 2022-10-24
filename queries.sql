2bb3f8ed717e150a65d7e10061ca53106c06fa68
John <kibirigejohn005@gmail.com>
2022-10-24 18:23:34 +0300

    add the first three queries

--- queries.sql
+++ queries.sql
@@ -250,3 +250,11 @@ species.id = (
 	) AS s_v
 	ON s_v.animal_id = animals.id
 );
+
+/* ------------------------------ WEEK TWO -----------------------------------------*/
+-- The following queries are taking too much time (1 sec = 1000ms can be considered as too much time for database query). Try them on your machine to confirm it
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+
+explain analyze SELECT * FROM visits where vet_id = 2;
+
+explain analyze SELECT * FROM owners where email = 'owner_18327@mail.com';
